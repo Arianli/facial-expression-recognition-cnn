8cfa98602e24c92f8fb4f3dddfba70d02a5bd7ec
Marian <140367174+Arianli@users.noreply.github.com>
2026-07-04 22:44:34 -0400

    Create model.py

---
+++ src/model.py
@@ -0,0 +1,15 @@
+"""
+CNN model definition for facial expression recognition.
+
+This file will contain the model architecture used for seven-category
+emotion classification.
+"""
+
+
+def build_model():
+    """
+    Build and return a CNN model for facial expression recognition.
+
+    This function will be implemented in a later step.
+    """
+    pass
